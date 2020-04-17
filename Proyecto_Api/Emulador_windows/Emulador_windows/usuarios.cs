566ec7baa263a519d30a0f1f40206979b9ec3ed0
Prieto21 <54006403+Prieto21@users.noreply.github.com>
2020-04-16 21:06:33 -0500

    Lanzamiento Del Proyecto Con Api
    Se lanza el nuevo modelo del proyecto, incluyendo el emulador de windows, donde se tambien se conectara con el archivo de rutas

---
+++ Proyecto_Api/Emulador_windows/Emulador_windows/usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emulador_windows
+{
+    public class usuarios
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string sexo { get; set; }
+        
+
+    }
+}
