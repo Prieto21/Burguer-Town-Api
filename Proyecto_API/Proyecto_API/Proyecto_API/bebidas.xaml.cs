4e3a750193ce58d021f2dc789e4210545302e18e
Prieto21 <54006403+Prieto21@users.noreply.github.com>
2020-04-17 17:56:45 -0500

    Vista Bebidas
    se anexo la vista de bebidas con el mismos diseño integrado del proyecto anterior, pero con la novedad de que la vista tiene conexion de base de datos y su respectiva consulta para solo "bebidas"

---
+++ Proyecto_API/Proyecto_API/Proyecto_API/bebidas.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyecto_API
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class bebidas : ContentPage
+    {
+        public bebidas()
+        {
+            InitializeComponent();
+            Getbebidas();
+        }
+
+        private async void Getbebidas()
+        {
+            HttpClient client = new HttpClient();
+            var pro = await client.GetStringAsync("http://localhost:3000/api/burguer/bebidas");
+            var bebi = JsonConvert.DeserializeObject<List<productos>>(pro);
+            bebida.ItemsSource = bebi;
+        }
+    }
+}
