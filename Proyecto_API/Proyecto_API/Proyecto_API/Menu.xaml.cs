5aa881eba2a8b142db0ab1354d5911bcf06f1c68
Prieto21 <54006403+Prieto21@users.noreply.github.com>
2020-04-17 17:40:22 -0500

    Vista De Hamburguesas (Menu)
    se anexo la vista anterior de hamburguesas del proyecto y al momento de implementarse en este nuevo proyecto se hizo la conexion de la  base de datos explicitamente con la consulta de "hamburguesas"

--- Proyecto_API/Proyecto_API/Proyecto_API/Menu.xaml.cs
+++ Proyecto_API/Proyecto_API/Proyecto_API/Menu.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,15 @@ namespace Proyecto_API
         public Menu()
         {
             InitializeComponent();
+            Gethamburguesas();
+        }
+
+        private async void Gethamburguesas()
+        {
+            HttpClient client = new HttpClient();
+            var pro = await client.GetStringAsync("http://localhost:3000/api/burguer/burger");
+            var bur = JsonConvert.DeserializeObject<List<productos>>(pro);
+            burguer.ItemsSource = bur;
         }
     }
 }
---
+++ Proyecto_API/Proyecto_API/Proyecto_API/productos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_API
+{
+    public class productos
+    {
+        public string nombre { get; set; }
+        public string imagen { get; set; }
+        public string valor { get; set; }
+    }
+}
