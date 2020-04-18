965747ca257f7afebbe8788a23c31442895fb74a
Prieto21 <54006403+Prieto21@users.noreply.github.com>
2020-04-17 19:36:53 -0500

    Vista Admin
    se crea la vista admin con un diseño explicitamente para ver los pedidos cancelados y los pedidos activos, donde obviamente ya estan conectados a la base de datos y cada uno con su consulta en especifico "activos" y "cancelados", juntos con sus funciones cada una

---
+++ Proyecto_API/Proyecto_API/Proyecto_API/administrador.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyecto_API
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class administrador : ContentPage
+    {
+        public administrador()
+        {
+            InitializeComponent();
+            GetpedidosActivos();
+            GetpedidosCancelados();
+        }
+
+        private async void GetpedidosCancelados()
+        {
+            HttpClient client = new HttpClient();
+            var pediC = await client.GetStringAsync("http://localhost:3000/api/burguer/cancel");
+            var cancel = JsonConvert.DeserializeObject<List<estadoo>>(pediC);
+            cancelado.ItemsSource = cancel;
+        }
+
+        private async void GetpedidosActivos()
+        {
+            HttpClient client = new HttpClient();
+            var pediA = await client.GetStringAsync("http://localhost:3000/api/burguer/activos");
+            var activados = JsonConvert.DeserializeObject<List<estadoo>>(pediA);
+            activo.ItemsSource = activados;
+        }
+    }
+}
